b8bab83fdc3b1c746b6f0e3782e1fdd2ed4af4b0
sdauuse <751648872@qq.com>
2021-01-07 14:55:43 +0800

    springmvc练习

---
+++ src/com/miao/springmvc/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.miao.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author miaoyin
+ * @date 2021/1/7 - 14:42
+ * @commet:
+ */
+@Controller
+public class MyController {
+
+}
